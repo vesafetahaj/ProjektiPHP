44372deb1f4a9c032447ff9a143f5d87485b12e6
vesafetahaj <vf58160@ubt-uni.net>
2023-01-31 22:29:16 +0100

    commit

---
+++ niveli1.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once('D:\xampp\htdocs\Projekti\ProjektiPHP\Professional-IT-School-1\html\niveli1.html');
+?>
---
+++ niveli2.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once('D:\xampp\htdocs\Projekti\ProjektiPHP\Professional-IT-School-1\html\niveli2.html');
+
+?>
