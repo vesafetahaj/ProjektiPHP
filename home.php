89275e8e3858ec9e1ea136377f327f7048d66e2d
vesafetahaj <vf58160@ubt-uni.net>
2023-02-20 14:11:14 +0100

    commit

--- home.php
+++ home.php
@@ -1,4 +1,177 @@
-<?php
-require_once('D:\xampp\htdocs\Projekti\ProjektiPHP\Professional-IT-School-1\html\home.html');
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>START SMART Academy</title>
+        <link rel="stylesheet" href="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/css/style-navbar.css">
+        <link href='https://fonts.googleapis.com/css?family=Alegreya Sans SC' rel='stylesheet'>
+        <link rel = "icon" href = "http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/start-smart-website-favicon-color.png" type = "image/x-icon">
+        <link rel="stylesheet" href="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/css/home.css">
+        <link href="https://fonts.cdnfonts.com/css/montblanc" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/css/footer.css">
+        <script src="https://kit.fontawesome.com/c2df117003.js" crossorigin="anonymous"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
 
-?>
+    <body>
+        <header id="header">
+            <a href="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/html/home.html"><img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/start-smart-high-resolution-logo-color-on-transparent-background.png" id="logo"></a>
+            <div id="socials">
+                <a href="https://www.facebook.com/" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+                <a href="https://www.instagram.com/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+                <a href="https://twitter.com/" target="_blank"><i class="fa-brands fa-twitter"></i></a>
+               <button id="darkmode"><i class="fa-solid fa-moon"></i></button>
+            </div>
+            <div id="nav">
+                <a href="http://localhost:8080/Projekti/ProjektiPHP/home.php">Home</a>
+                <a href="http://localhost:8080/Projekti/ProjektiPHP/planprogrami.php">Planprogrami</a>
+                <a href="http://localhost:8080/Projekti/ProjektiPHP/news.php">News</a>
+                <a href="http://localhost:8080/Projekti/ProjektiPHP/aboutus.php">About Us</a>
+                <a href="http://localhost:8080/Projekti/ProjektiPHP/database/view/contact.php">Contact</a>
+                <a href="http://localhost:8080/Projekti/ProjektiPHP/regjistrohu.php">Regjistrohu</a>
+            </div>
+            <button id="loginbttn" onclick="location.href='http://localhost:8080/Projekti/ProjektiPHP/login validation/login.php'">Your Projects</button>
+        </header>
+        <main>
+            <div class="slideshow-container">
+            
+                <div class="mySlides fade">
+                  <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/pic1.jpg" style="width:134%">
+                </div>
+                
+                <div class="mySlides fade">
+                  <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/Child_programming_on_Pictoblox_395d374534 (2).jpg" style="width:134%;">
+                </div>
+                
+                <div class="mySlides fade">
+                  <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/idsfhh123_1.jpg" style="width:134%">
+                </div>
+
+                <div class="mySlides fade">
+                    <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/EAL-NB.jpg" style="width:134%">
+                  </div>
+ 
+            </div>
+            
+            <div class="kurrikula-body">
+                <h3>Kurrikula</h3>
+                <div class="Kurrikula-right">
+                    <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/circle (1).png">
+                    <div class="content1">
+                        <h1 class="kurrikula-content-right">Logjika e 
+                            <br>
+                            <span class="second-title">Programimit</span>
+                        </h1>
+                        
+                    </div>
+                </div>
+              
+                <div class="Kurrikula-left">
+                    <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/dry-clean.png">
+                    <div class="content1">
+                        <h1 class="kurrikula-content-left">Webfaqe &
+                            <br>
+                            <span class="second-title">Aplikacione</span>
+                        </h1>
+                    </div>
+                </div>
+               
+                <div class="Kurrikula-right">
+                    <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/circle.png">
+                    <div class="content1">
+                        <h1 class="kurrikula-content-right">Programim i  
+                            <br>
+                            <span class="second-title">Avancuar</span>
+                        </h1>
+                    </div>
+                  
+                </div>
+                <div class="Kurrikula-left">
+                    <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/oval.png">
+                    <div class="content1">
+                        <h1 class="kurrikula-content-left">Programim
+                            <br>
+                            <span class="second-title">Profesional</span>
+                        </h1>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="about-body">
+                <div class="about-img">
+                    <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/llaptopi-8-255x255.png" id="llaptop">
+                </div>
+                <div class="about-content" id="about-content">
+                    <h2>Rreth planprogramit</h2> <br>
+                    <em>
+                    <?php
+
+                        require_once 'D:\xampp\htdocs\Projekti\ProjektiPHP\database\database\databaseConnection.php';
+                        $db = new DBConnection();
+                        $conn = $db->startConnection();
+                         
+                        $stmt = $conn->prepare("SELECT text FROM home WHERE id = 10");
+                        $stmt->execute();
+                        $result = $stmt->fetch();
+
+                    
+                        echo $result['text'];
+
+                        ?>
+                    </em><br>
+                    <button id="readbttn" onclick="location.href='http://localhost:8080/Projekti/ProjektiPHP/planprogrami.php'">Read More ➔</button>
+                </div>
+            </div>
+    
+            <div class="gallery">
+                <div class="gallery-photo">
+                    <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/Teacher-helping-3-high-school-students-with-project-Feature-Image.jpg" width="600" height="400">
+                </div>
+                
+                <div class="gallery-photo">
+                    <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/Coding_Image_Top.jpg" width="600" height="400">
+                </div>
+
+                <div class="gallery-photo">
+                        <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/cute-pupils-in-computer-class-with-teacher-1024x512.jpg" width="600" height="400">
+                </div>
+                
+                <div class="gallery-photo">
+                    <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/Minecraft-Tutorial-img3-1240x736.jpg" width="600" height="400">
+                </div>
+                
+                <div class="gallery-photo">
+                    <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/EAL-NB.jpg" width="600" height="400">
+                </div>
+
+                <div class="gallery-photo">
+                        <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/minecraftNB-600x337.jpg" width="600" height="400">
+                </div>
+                
+                <div class="gallery-photo">
+                    <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/tok-pic-NB-600x337.jpg" width="600" height="400">
+                </div>
+
+                <div class="gallery-photo">
+                    <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/personal-NB-600x337.jpg" width="600" height="400">
+                </div>
+            </div>
+        </main>
+        <footer>
+            <a href="http://localhost:8080/Projekti/ProjektiPHP/home.php"><img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/start-smart-website-favicon-white.png"></a>
+            <div class="first-content">
+                <a href="https://www.google.com/maps/place/Universiteti+p%C3%ABr+Biznes+dhe+Teknologji/@42.6026634,21.108897,13z/data=!4m8!1m2!2m1!1subt!3m4!1s0x13549e8d5d607f25:0xa31dd05b21bd09de!8m2!3d42.647155!4d21.153124"><p>Lagjja Kalabria,10000 Prishtine, Kosovo</p></a>
+            </div>
+            <div class="second-content">
+                <p>+383 38 541 400</p>
+            </div>
+            <div class="third-content">
+                <a href="https://mail.google.com/"><p>startsmartacademy@gmail.com</p></a>
+            </div>
+            <p id="legal">© COPYRIGHT 2022. START SMART ACADEMY. ALL RIGHTS RESERVED.</p>
+        </footer>
+        <script src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/javascript/home.js"></script>
+    </body>
+</html>
--- planprogrami.php
+++ planprogrami.php
@@ -1,4 +1,185 @@
-<?php
-require_once 'D:\xampp\htdocs\Projekti\ProjektiPHP\Professional-IT-School-1\html\planprogrami.html';
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Planprogrami - START SMART ACADEMY</title>
+        <link rel="stylesheet" href="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/css/style-navbar.css">
+        <link href='https://fonts.googleapis.com/css?family=Alegreya Sans SC' rel='stylesheet'>
+        <link rel = "icon" href = "http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/start-smart-website-favicon-color.png" type = "image/x-icon">
+        <link rel="stylesheet" href="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/css/planprogrami.css">
+        <link rel="stylesheet" href="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/css/footer.css">
+        <script src="https://kit.fontawesome.com/c2df117003.js" crossorigin="anonymous"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <header id="header">
+            <a href="http://localhost:8080/Projekti/ProjektiPHP/home.php"><img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/start-smart-high-resolution-logo-color-on-transparent-background.png" id="logo"></a>
+            <div id="socials">
+                <a href="https://www.facebook.com/" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+                <a href="https://www.instagram.com/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+                <a href="https://twitter.com/" target="_blank"><i class="fa-brands fa-twitter"></i></a>
+            </div>
+            <div id="nav">
+                <a href="http://localhost:8080/Projekti/ProjektiPHP/home.php">Home</a>
+                <a href="http://localhost:8080/Projekti/ProjektiPHP/planprogrami.php" class="activeNav">Planprogrami</a>
+                <a href="http://localhost:8080/Projekti/ProjektiPHP/news.php">News</a>
+                <a href="http://localhost:8080/Projekti/ProjektiPHP/aboutus.php">About Us</a>
+                <a href="http://localhost:8080/Projekti/ProjektiPHP/database/view/contact.php">Contact</a>
+                <a href="http://localhost:8080/Projekti/ProjektiPHP/regjistrohu.php">Regjistrohu</a>
+            </div>
+            <button id="loginbttn" onclick="location.href='http://localhost:8080/Projekti/ProjektiPHP/login validation/login.php'">Your Projects</button>
+        </header>
+        <main>
+            <div class="img-container">
+                <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/image.png">
+            </div>
+            <div class="text-container">
+                <h1>Metodologjia e
+                    <br> mësimdhënies
+                </h1>   
+                <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/Animated-Down-Arrow.gif" onclick="location.href='#main-content'">
+            </div>
+            <div id="main-content"><!--id per shkak te shigjetes-->
+                <div class="img-content" style="float: left; margin: 0;">
+                    <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/size0-full.jpg">
+                </div>
+                <div class="text-content" id="text-content1" style="float: right; margin: 0;">
+                    <h1>MBAJTJA E MËSIMIT <br>FIZIKISHT OSE ONLINE</h1>
+                    <h3>Start Smart Academy ofron mundësinë për mësim fizikisht dhe online. 
+                    Mësimi organizohet me intensitet 2 herë në javë me nga 1 orë e 
+                    gjysmë akademike, duke zgjedhur orarin që ju përshtatet më së shumti.
+                    Nxënësit punojnë nën udhëheqjen e instuktorëve, në mënyrë praktike duke 
+                    aplikuar të gjitha njohuritë e nxëna, në hapesirat e Start Smart Academy 
+                    e cila plotëson të gjitha kushtet e nevojshme me teknologjitë më të reja. 
+                    Me anën e projekteve dhe vlerësimit sistematik nxënësit kalojnë nga një
+                    nivel në tjetrin.</h3>
+                </div>
+                <div class="text-content" id="text-content2" style="float: right; margin: 0;">
+                    <h1>MËSIMI DHE ARGËTIM</h1>
+                    <h3>Start Smart Academy aplikon metodologjinë më të re të mësimdhënies në botë - Gamifikimin,
+                        ku fëmijët përmes angazhimit të vazhdueshëm sikurse në lojëra, do të kalojnë në nivele, 
+                        nga ai bazik deri te ai i avancuar duke u vlerësuar përmes pikëve të eksperiencës ose XP 
+                        (Experience Points). Këto pikë, fëmijët i fitojnë si vlerësim i punës së tyre gjatë periudhës 
+                        sa ata mësojnë, kur përfundojnë detyra dhe sfida të ndryshme.<br><br>
+                        Nxënësit ndahen në katër kategori (houses), në bazë të veçorive që zotojnë, kështu ata duke 
+                        kryer aktivitete dhe duke marr pjesë në gara të ndryshme për të fituar sa më shumë XP, 
+                        bëhen pjesë e rrjetit ndërkombëtarë të nxënësve të Start Smart Academy në vende të ndryshme.   
+                    </h3>
+                </div>
+                <div class="img-content" style="float: left; margin:0;">
+                    <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/Community-Day.jpg">
+                </div>
+                
+                <div class="img-content" style="float: left; margin: 0;">
+                    <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/adult-teaching-kids-coding-scratch.jpeg">
+                </div>
+                <div class="text-content" id="text-content3" style="float: right; margin: 0;">
+                    <h1>EKIPI I KUALITETIT</h1><br>
+                    <h3>Me qëllim që mësimi të jetë sa më efektiv dhe kualitativ, ekipi i kualitetit monitoron secilin 
+                        grup gjatë mësimit fizikisht apo online duke vlerësuar projektet e nxënësve në llogarinë e tyre 
+                        në platformën LMS, kështu duke siguruar që projektet plotësojnë kriteret e caktuara në bazë të 
+                        niveleve përkatëse.<br>Nxënësit të cilët nuk përmbushin kriteret e caktuara nga Start Smart Academy, 
+                        rekomandohen të përsërisin projektet ose nivelin gjithnjë duke u konsultuar edhe me prindërit e tyre.</h3>
+                </div>
+            </div>
+            <div class="planprogrami-left" id="firstPlanprogram">
+                <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/istockphoto-923729440-612x612-removebg-preview.png">
+                 <h2>KURRIKULA PËR FËMIJËT 8-11 VJEÇ</h2>
+                 <h3><?php
 
-?>
+                    require_once 'D:\xampp\htdocs\Projekti\ProjektiPHP\database\database\databaseConnection.php';
+                    $db = new DBConnection();
+                    $conn = $db->startConnection();
+                    
+                    $stmt = $conn->prepare("SELECT kurrikula8deri11 FROM planprogrami WHERE id = 1");
+                    $stmt->execute();
+                    $result = $stmt->fetch();
+
+
+                    echo $result['kurrikula8deri11'];
+
+                ?></h3><br>
+                    <a href="http://localhost:8080/Projekti/ProjektiPHP/niveli1.php" class="meshume">MË SHUMË</a>
+            </div>
+            <div class="planprogrami-right">
+                <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/kids-glasses_blue-sky-blue-transparent-full-rim-cat-eyes-5-8-yrs-hooper-astra-hooper-c3_24_nov_kids_shoot183738_149978_29_nov_2022-removebg-preview.png" id="boyGlasses">
+                 <h2>KURRIKULA PËR FËMIJËT 12-15 VJEÇ</h2>
+                 <h3><?php
+
+                    require_once 'D:\xampp\htdocs\Projekti\ProjektiPHP\database\database\databaseConnection.php';
+                    $db = new DBConnection();
+                    $conn = $db->startConnection();
+                    
+                    $stmt = $conn->prepare("SELECT kurrikula12deri15 FROM planprogrami WHERE id = 1");
+                    $stmt->execute();
+                    $result = $stmt->fetch();
+
+
+                    echo $result['kurrikula12deri15'];
+
+                ?></h3><br>
+                    <a href="http://localhost:8080/Projekti/ProjektiPHP/niveli2.php" class="meshume">MË SHUMË</a>
+            </div>
+            <div class="planprogrami-left" style="background-color: rgb(108, 187, 108); margin-top: -30px;" id="planprogrami-green">
+                <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/640-05761084en_Masterfile-removebg-preview (1).png" id="boyGreen">
+                 <h2>KURRIKULA PËR FËMIJËT 16-18 VJEÇ</h2>
+                 <h3><?php
+
+                    require_once 'D:\xampp\htdocs\Projekti\ProjektiPHP\database\database\databaseConnection.php';
+                    $db = new DBConnection();
+                    $conn = $db->startConnection();
+                    
+                    $stmt = $conn->prepare("SELECT kurrikula16deri18 FROM planprogrami WHERE id = 1");
+                    $stmt->execute();
+                    $result = $stmt->fetch();
+
+
+                    echo $result['kurrikula16deri18'];
+
+                ?></h3><br>
+                    <a href="http://localhost:8080/Projekti/ProjektiPHP/niveli3.php" class="meshume">MË SHUMË</a>
+            </div>
+            <div class="head-activities">
+                <h3>Other activities</h3>
+            </div>
+           <div class="activities">
+                <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/forum.png" class="reveal">
+                <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/cinema.png" class="reveal">
+                <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/games.png" class="reveal">
+                <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/quiz.png" class="reveal">
+                <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/awards.png" class="reveal">
+                <img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/hackathon-200x150.png" class="reveal">
+           </div>
+           <div class="activities-headings">
+                <h3 style="padding-left: 40px;" class="reveal">Forum</h3>
+                <h3 style="padding-left: 80px;"class="reveal">Cinema</h3>
+                <h3 style="text-align: center; padding-left: 30px;"class="reveal">E-games <br>Tournament</h3>
+                <h3 class="reveal" id="quiz">Knowledge Quiz</h3>
+                <h3 class="reveal" id="award">Awards</h3>
+                <h3 class="reveal" id="hackathon">Hackathon</h3>
+           </div>
+           <div class="activities-content">
+                <h6 class="reveal">Fëmijët bashkëpunojnë, ndajnë punën e tyre dhe bëjnë pyetje në forum.</h6>
+                <h6 class="reveal">Një herë në javë në ambientet e shkollës organizohet kinema</h6>
+                <h6 class="reveal">Një herë në vit zhvillohet turneu më i madh i e-sporteve për fëmijë ku fëmijët konkurrojnë në lojëra të ndryshme kompjuterike</h6>
+                <h6 class="reveal">Një herë në vit të gjithë fëmijët konkurrojnë në kuizin e njohurive</h6>
+                <h6 class="reveal">Fëmijët garojnë me projektet e tyre gjatë gjithë vitit ku në fund marrin çmime të ndryshme.</h6>
+                <h6 class="reveal">Pas përfundimit të vitit shkollor, fëmijët konkurrojnë në një sesion kodimi 4 orësh dhe më pas prezantojnë punën e tyre në një seancë transmetimi në internet</h6>
+           </div>
+            
+        </main>
+        <footer>
+            <a href="http://localhost:8080/Projekti/ProjektiPHP/home.php"><img src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/images/start-smart-website-favicon-white.png"></a>
+            <div class="first-content">
+                <a href="https://www.google.com/maps/place/Universiteti+p%C3%ABr+Biznes+dhe+Teknologji/@42.6026634,21.108897,13z/data=!4m8!1m2!2m1!1subt!3m4!1s0x13549e8d5d607f25:0xa31dd05b21bd09de!8m2!3d42.647155!4d21.153124"><p>Lagjja Kalabria,10000 Prishtine, Kosovo</p></a>
+            </div>
+            <div class="second-content">
+                <p>+383 38 541 400</p>
+            </div>
+            <div class="third-content">
+                <a href="https://mail.google.com/"><p>startsmartacademy@gmail.com</p></a>
+            </div>
+            <p id="legal">© COPYRIGHT 2022. START SMART ACADEMY. ALL RIGHTS RESERVED.</p>
+        </footer>
+        <script src="http://localhost:8080/Projekti/ProjektiPHP/Professional-IT-School-1/javascript/planprogrami.js"></script>
+    </body>
+</html>
